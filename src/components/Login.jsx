a7b2a3dbdad3e7cd38cd72127233b91ae383f299
Priyanku Saikia <123909182+Priyanku-AI@users.noreply.github.com>
2025-07-27 20:37:20 +0530

    using styled components

---
+++ src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Login() {
+    return (
+        <Div>Login</Div>
+    )
+}
+
+const Div = styled.div`
+background-color: red;
+`
+
+export default Login
